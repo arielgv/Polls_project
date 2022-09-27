95dc283bcd323284b26122885e6922e11247ee9e
Ariel Villafane <ariel.g.villafane@gmail.com>
2022-09-27 12:23:59 -0300

    corrections

--- premiosapp/polls/models.py
+++ premiosapp/polls/models.py
@@ -5,7 +5,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=250)
     pub_date= models.DateTimeField("publication date")
 
-class Choices(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=250)
     votes = models.IntegerField(default=0)
