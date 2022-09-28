41050a489b4e132bb5afe963f2d5811e76006218
Ariel Villafane <ariel.g.villafane@gmail.com>
2022-09-28 16:50:16 -0300

    variables arregladas

--- premiosapp/polls/models.py
+++ premiosapp/polls/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=250)
     pub_date= models.DateTimeField("publication date")
 
+    def __str__(self):
+        return self.question_text
+    
+    def recently_published(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=250)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text
