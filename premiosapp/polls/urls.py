9224853e295486b6e35e729438966223e13a8e7d
Ariel Villafane <ariel.g.villafane@gmail.com>
2022-09-29 22:46:51 -0300

    Bugs fixed

--- premiosapp/polls/urls.py
+++ premiosapp/polls/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
+#from .models import Question,Choice
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+
+    path("", views.index, name="index"),
+    path("<int:question>", views.detail, name="index"),
+    path("<int:question>/results/", views.results, name="index"),
+    path("<int:question>/vote/", views.vote, name="index")
+
 ]
--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#from polls.models import Question, Choice
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("This is the main page.")
+
+
+def detail(request,question):
+    return HttpResponse(f'Your primary key is {question} ' )
+
+
+def results(request,question):
+    return HttpResponse(f'You are looking the results of Question {question} ' )
+
+
+def vote(request,question):
+    return HttpResponse(f'You are voting the question number {question} ' )
